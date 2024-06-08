14fcaf7622a70aaad2c9229747a7e80f624f68a5
gioboa <giorgiob.boa@gmail.com>
2024-06-09 01:58:34 +0200

    chore: update deps

--- remote/src/root.tsx
+++ remote/src/root.tsx
@@ -4,17 +4,17 @@ import { QwikCityProvider, RouterOutlet } from '@builder.io/qwik-city';
 import './global.css';
 
 export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCityProvider> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Don't remove the `<head>` and `<body>` elements.
-   */
+	/**
+	 * The root of a QwikCity site always start with the <QwikCityProvider> component,
+	 * immediately followed by the document's <head> and <body>.
+	 *
+	 * Don't remove the `<head>` and `<body>` elements.
+	 */
 
-  return (
-    <QwikCityProvider>
-      <RouterOutlet />
-      <PrefetchGraph />
-    </QwikCityProvider>
-  );
+	return (
+		<QwikCityProvider>
+			<RouterOutlet />
+			<PrefetchGraph base="/remote/build/" />
+		</QwikCityProvider>
+	);
 });
