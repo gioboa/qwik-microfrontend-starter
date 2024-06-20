aab8dadd6da631b5da360da0d8acbba2815cb088
PatrickJS <github@patrickjs.com>
2024-06-20 05:35:53 -0700

    fix: remote to local proxy (#2)

--- host/src/components/RemoteMfe.tsx
+++ host/src/components/RemoteMfe.tsx
@@ -12,29 +12,28 @@ export interface Props {
 export default component$(({ remote, removeLoader = false }: Props) => {
 	const url = remote.url;
 	const decoder = new TextDecoder();
-	const getSSRStreamFunction =
-		(remoteUrl: string) => async (stream: StreamWriter) => {
-			const _remoteUrl = new URL(remoteUrl);
-			if (removeLoader) {
-				_remoteUrl.searchParams.append('loader', 'false');
-			}
-			const reader = (
-				await fetch(_remoteUrl, {
-					headers: {
-						accept: 'text/html',
-					},
-				})
-			).body!.getReader();
-			let fragmentChunk = await reader.read();
-			let base = '';
-			while (!fragmentChunk.done) {
-				const rawHtml = decoder.decode(fragmentChunk.value);
-				const fixedHtmlObj = fixRemotePathsInDevMode(rawHtml, base);
-				base = fixedHtmlObj.base;
-				stream.write(fixedHtmlObj.html);
-				fragmentChunk = await reader.read();
-			}
-		};
+	const getSSRStreamFunction = (remoteUrl: string) => async (stream: StreamWriter) => {
+		const _remoteUrl = new URL(remoteUrl);
+		if (removeLoader) {
+			_remoteUrl.searchParams.append('loader', 'false');
+		}
+		const reader = (
+			await fetch(_remoteUrl, {
+				headers: {
+					accept: 'text/html',
+				},
+			})
+		).body!.getReader();
+		let fragmentChunk = await reader.read();
+		let base = '';
+		while (!fragmentChunk.done) {
+			const rawHtml = decoder.decode(fragmentChunk.value);
+			const fixedHtmlObj = fixRemotePathsInDevMode(rawHtml, base);
+			base = fixedHtmlObj.base;
+			stream.write(fixedHtmlObj.html);
+			fragmentChunk = await reader.read();
+		}
+	};
 
 	return (
 		<SSRStreamBlock>
--- remote/src/entry.ssr.tsx
+++ remote/src/entry.ssr.tsx
@@ -10,36 +10,33 @@
  * - npm run build
  *
  */
-import {
-  renderToStream,
-  type RenderToStreamOptions,
-} from '@builder.io/qwik/server';
+import { renderToStream, type RenderToStreamOptions } from '@builder.io/qwik/server';
 import { manifest } from '@qwik-client-manifest';
 import Root from './root';
 
 const base = '/remote/build/';
 
 export default function (opts: RenderToStreamOptions) {
-  const url = new URL(opts.serverData!.url);
-  return renderToStream(<Root />, {
-    manifest,
-    base,
-    ...opts,
-    // Use container attributes to set attributes on the html tag.
-    containerAttributes: {
-      lang: 'en-us',
-      ...opts.containerAttributes,
-    },
-    prefetchStrategy: {
-      implementation: {
-        linkInsert: null,
-        workerFetchInsert: null,
-        prefetchEvent: 'always',
-      },
-    },
-    containerTagName: 'div',
-    qwikLoader: {
-      include: url.searchParams.get('loader') ? 'never' : 'auto',
-    },
-  });
+	const url = new URL(opts.serverData!.url);
+	return renderToStream(<Root />, {
+		manifest,
+		base,
+		...opts,
+		// Use container attributes to set attributes on the html tag.
+		containerAttributes: {
+			lang: 'en-us',
+			...opts.containerAttributes,
+		},
+		prefetchStrategy: {
+			implementation: {
+				linkInsert: null,
+				workerFetchInsert: null,
+				prefetchEvent: 'always',
+			},
+		},
+		containerTagName: 'div',
+		qwikLoader: {
+			include: url.searchParams.get('loader') ? 'never' : 'auto',
+		},
+	});
 }
